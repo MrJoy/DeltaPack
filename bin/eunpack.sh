5bf8bf4197c09296d3a07782c93a1b2861fea6b4
Jon Frisby <jfrisby@mrjoy.com>
2013-04-18 02:33:58 -0700

    A prototype EDelta compressor...

---
+++ bin/eunpack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PACKAGE="$1"
+
+if [ ! -e "$PACKAGE" ]; then
+  echo "Must specify a package."
+  exit 1
+fi
+
+tar xjf "$PACKAGE"
+(
+  cd "${PACKAGE%.tar.bz2}"
+  while [ 1 ]; do
+
+    BASIS=$(ls | grep -v -E '\.epatch$' | sort -n | tail -1 2>/dev/null)
+    if [ ! -e "$BASIS" ]; then
+      echo "Couldn't find a basis file to work from."
+      exit 1
+    fi
+    DIFF=$(ls ${BASIS}_*.epatch 2> /dev/null)
+    SUFFIX=${DIFF#${BASIS}_}
+    TARGET=${SUFFIX%.epatch}
+
+    if [ -e "$DIFF" ]; then
+      echo "Rebuilding $TARGET via $BASIS and $DIFF..."
+      edelta patch $BASIS $TARGET $PATCH && rm $DIFF
+    else
+      echo "Looks like we're done here."
+      exit 0
+    fi
+  done
+)
