0d5bab55bc82e11137f8c80b3b012199591c9516
Jon Frisby <jfrisby@mrjoy.com>
2013-04-18 07:32:22 -0700

    Comment this out for now...

--- lib/deltapack/encoders/zdelta.rb
+++ lib/deltapack/encoders/zdelta.rb
@@ -1,49 +1,52 @@
-module DeltaPack
-  module Encoders
-    class ZDelta < DeltaPack::Encoder
-      KIND = :zdelta
+# Disabled until I can suss out why the fuck this is producing a segault,
+# *reliably*, on some decompressions!
 
-      def encode(basis_name, target_name)
-        delta_name = File.join(self.temp_dir, "zdelta.tmp")
-        begin
-          result = system(
-            "zdc",
-            basis_name,
-            target_name,
-            delta_name
-          )
-          result = IO.binread(delta_name) if(result)
+# module DeltaPack
+#   module Encoders
+#     class ZDelta < DeltaPack::Encoder
+#       KIND = :zdelta
 
-          return result
-        ensure
-          File.unlink(delta_name)
-        end
-      end
+#       def encode(basis_name, target_name)
+#         delta_name = File.join(self.temp_dir, "zdelta.tmp")
+#         begin
+#           result = system(
+#             "zdc",
+#             basis_name,
+#             target_name,
+#             delta_name
+#           )
+#           result = IO.binread(delta_name) if(result)
 
-      def decode(basis_contents, delta_contents)
-        begin
-          basis_name = File.join(self.temp_dir, "zdelta.basis")
-          IO.binwrite(basis_name, basis_contents)
-          delta_name = File.join(self.temp_dir, "zdelta.delta")
-          IO.binwrite(delta_name, delta_contents)
-          target_name = File.join(self.temp_dir, "zdelta.target")
+#           return result
+#         ensure
+#           File.unlink(delta_name)
+#         end
+#       end
 
-          result = system(
-            "zdu",
-            basis_name,
-            delta_name,
-            target_name
-          )
+#       def decode(basis_contents, delta_contents)
+#         begin
+#           basis_name = File.join(self.temp_dir, "zdelta.basis")
+#           IO.binwrite(basis_name, basis_contents)
+#           delta_name = File.join(self.temp_dir, "zdelta.delta")
+#           IO.binwrite(delta_name, delta_contents)
+#           target_name = File.join(self.temp_dir, "zdelta.target")
 
-          result = IO.binread(target_name) if(result)
+#           result = system(
+#             "zdu",
+#             basis_name,
+#             delta_name,
+#             target_name
+#           )
 
-          return result
-        ensure
-          File.unlink(target_name) rescue nil
-          File.unlink(basis_name) rescue nil
-          File.unlink(delta_name) rescue nil
-        end
-      end
-    end
-  end
-end
+#           result = IO.binread(target_name) if(result)
+
+#           return result
+#         ensure
+#           File.unlink(target_name) rescue nil
+#           File.unlink(basis_name) rescue nil
+#           File.unlink(delta_name) rescue nil
+#         end
+#       end
+#     end
+#   end
+# end
